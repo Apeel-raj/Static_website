5ebe4e0cb18be9dfd944c0d85ab50955bbc998ba
Apeel-raj <apeeladhikari66@gmail.com>
2026-03-21 10:53:34 +0545

    updated the price and form on firebase

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -16,6 +16,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const storage = getStorage(app);
 
-export { db, storage };
+export { db };
